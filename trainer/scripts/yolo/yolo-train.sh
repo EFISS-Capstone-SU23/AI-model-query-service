a5030d23da386af41de82b745e3ac5772a36bef6
thaiminhpv <thaiminhpv@gmail.com>
2023-07-23 02:27:01 +0700

    train yolo using gpu

--- trainer/scripts/yolo/yolo-train.sh
+++ trainer/scripts/yolo/yolo-train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # https://docs.ultralytics.com/modes/train/#usage-examples
-yolo detect train data=data/deepfashion2.yaml model=yolov8n.pt epochs=100 imgsz=640 device=cpu workers=64
+yolo detect train data=data/deepfashion2.yaml model=yolov8n.pt epochs=100 imgsz=640 device=0 workers=64
