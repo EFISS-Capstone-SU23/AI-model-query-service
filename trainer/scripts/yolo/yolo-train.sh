a84c112a7241c056084d54d22a1eeba2d8ac85db
thaiminhpv <thaiminhpv@gmail.com>
2023-07-23 22:31:38 +0700

    train yolo

--- trainer/scripts/yolo/yolo-train.sh
+++ trainer/scripts/yolo/yolo-train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # https://docs.ultralytics.com/modes/train/#usage-examples
-yolo detect train data=data/deepfashion2.yaml model=yolov8n.pt epochs=100 imgsz=640 device=0 workers=64
+yolo detect train data=data/deepfashion2.yaml model=yolov8n.pt epochs=12 imgsz=640 device=0 workers=64 batch=-1
