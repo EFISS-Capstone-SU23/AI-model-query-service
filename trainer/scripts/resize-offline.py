424d72cbc6e759ecb21e2746bb8e42d99532d25d
thaiminhpv <thaiminhpv@gmail.com>
2023-07-24 08:16:42 +0700

    fix error prone resize-offline

--- trainer/scripts/resize-offline.py
+++ trainer/scripts/resize-offline.py
@@ -10,7 +10,7 @@ import albumentations as A
 import pandas as pd
 import json
 
-SIZE = 600
+SIZE = 224
 
 def rename_path_to_original_extension(img_path: str) -> Optional[str]:
     try:
@@ -29,21 +29,26 @@ def rename_path_to_original_extension(img_path: str) -> Optional[str]:
         print(e)
         return None
 
-with open('database_info.txt', 'r') as f:
+with open('_database_info.txt', 'r') as f:
     lines = f.readlines()
     img_paths = [line.strip() for line in lines]
 
 corrupted_images = []
 resized_img_paths = []
 
+source = 'output/'
+
 print("Begin resizing images...")
 for file_path in tqdm(img_paths):
-    __new_path = file_path.replace('output/', f'resize_{SIZE}x{SIZE}/')
+    __new_path = file_path.replace(source, f'resize_{SIZE}x{SIZE}/')
+    if __new_path == file_path:
+        raise ValueError(f"Source path {file_path} is not in {source}")
     if os.path.exists(__new_path):
         resized_img_paths.append(__new_path)
-        print(f"Skip resized image: {file_path}")
+        print(f"Skip resized image: {__new_path}")
         continue
     _file_path = rename_path_to_original_extension(file_path)
+    _file_path = file_path
     if _file_path is None:
         print(f"Corrupted image: {file_path}")
         corrupted_images.append(file_path)
@@ -53,6 +58,7 @@ for file_path in tqdm(img_paths):
     file_path = _file_path
     abs_path = os.path.abspath(file_path)
     try:
+
         if file_path.endswith('.jpeg'):
             with open(abs_path, 'rb') as f:
                 check_chars = f.read()[-2:]
@@ -63,7 +69,7 @@ for file_path in tqdm(img_paths):
         
         img = cv2.imread(abs_path)
         img = cv2.resize(img, (SIZE, SIZE))
-        new_path = file_path.replace('output/', f'resize_{SIZE}x{SIZE}/')
+        new_path = file_path.replace(source, f'resize_{SIZE}x{SIZE}/')
         os.makedirs(os.path.dirname(new_path), exist_ok=True)
         result = cv2.imwrite(new_path, img)
         if not result:
@@ -83,5 +89,5 @@ print("coprrupted images: ", corrupted_images)
 with open(f'resized_{SIZE}x{SIZE}_database_info.txt', 'w') as f:
     f.write('\n'.join(resized_img_paths))
 
-with open('corrupted_images.txt', 'w') as f:
-    f.write('\n'.join(corrupted_images))
+# with open('corrupted_images.txt', 'w') as f:
+#     f.write('\n'.join(corrupted_images))
