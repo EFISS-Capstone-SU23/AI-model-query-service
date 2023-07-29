7c66ece7a8e1d9e018231d0ccb6642f3e6241548
thaiminhpv <thaiminhpv@gmail.com>
2023-07-29 18:02:34 +0700

    update to syncing from GCS to local instead

--- auto.sh
+++ auto.sh
@@ -4,9 +4,12 @@
 echo ----- $(date) -------
 
 cd /home/saplab/thaiminhpv/EFISS/AI-model-query-service
+
+echo "Start syncing data from GCS"
+gsutil -m rsync -r gs://efiss/data/product_images /home/saplab/thaiminhpv/relahash/rsi-prototype/data/shopee_29-7/product_images > /dev/null 2>&1
+echo Done, begin indexing
+
 bash indexer/increase_version.sh
 bash indexer/reindex.sh
 
-echo "Start syncing data to GCS"
-gsutil -m rsync -r /home/saplab/thaiminhpv/relahash/rsi-prototype/data/shopee_29-7/product_images gs://efiss/data/product_images > /dev/null 2>&1
-echo Done
+echo Done!
