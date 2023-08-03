14e0b6f90bd1ebd92faf625d1a5385fc4e2901ad
thaiminhpv <thaiminhpv@gmail.com>
2023-08-03 15:27:09 +0700

    offline inference vit

--- trainer/notebooks/vit/script.py
+++ trainer/notebooks/vit/script.py
@@ -5,6 +5,7 @@ from transformers import ViTImageProcessor, ViTForImageClassification, ViTFeatur
 from PIL import Image
 import torch
 import torch.nn as nn
+import faiss
 
 processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
 model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
@@ -24,35 +25,40 @@ df = pd.read_csv('data/shopee_crop_yolo/database.txt', sep=' ', nrows=None, name
 df
 from datasets import Dataset
 dataset = Dataset.from_pandas(df)
-dataset
+
+chunk_size = 100000
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.eval()
+model.to(device)
+
 def tokenize_function(examples):
     img = cv2.imread(examples["img_path"])
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     inputs = processor(images=img, return_tensors="pt")
     inputs['pixel_values'] = inputs['pixel_values'].squeeze()
     return inputs
-tokenized_dataset = dataset.map(tokenize_function, batched=False, batch_size=100, writer_batch_size=100)
-tokenized_dataset = tokenized_dataset.remove_columns(['img_path'])
-tokenized_dataset.save_to_disk("/media/saplab/MinhNVMe/relahash/temp/tokenized_dataset.hf")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-tokenized_dataset = tokenized_dataset.with_format("torch", device=device)
-dataloader = torch.utils.data.DataLoader(tokenized_dataset, batch_size=32, shuffle=False)
-model.eval()
-...
-# logits.logits.shape
-model.to(device)
-embeddings = []
-with torch.no_grad():
-    for i, x in enumerate(tqdm(dataloader)):
-        logits = model(**x).logits
-        embeddings.append(logits.cpu())
-    embeddings = torch.cat(embeddings)
-embeddings.shape
-import faiss
-import torch
 
-# quantizer = faiss.IndexFlatL2(768)
+for chunk in tqdm(range(0, len(dataset), chunk_size)):
+    print(f"Processing chunk {chunk}")
+    tokenized_dataset = Dataset.from_dict(dataset[chunk:chunk+chunk_size]).map(tokenize_function, batched=False, batch_size=100, writer_batch_size=100)
+    tokenized_dataset = tokenized_dataset.remove_columns(['img_path'])
+    tokenized_dataset.save_to_disk(f"/media/saplab/MinhNVMe/relahash/temp/tokenized_dataset_{chunk}.hf")
 
-torch.save({
-    "embedding": embeddings
-}, '/media/saplab/MinhNVMe/relahash/temp/embeddings.pth')
+    tokenized_dataset = tokenized_dataset.with_format("torch", device=device)
+    dataloader = torch.utils.data.DataLoader(tokenized_dataset, batch_size=512, shuffle=False)
+
+    embeddings = []
+    with torch.no_grad():
+        for i, x in enumerate(tqdm(dataloader)):
+            logits = model(**x).logits
+            embeddings.append(logits.cpu())
+        embeddings = torch.cat(embeddings)
+
+    print(embeddings.shape)
+
+    torch.save({
+        "embedding": embeddings
+    }, '/media/saplab/MinhNVMe/relahash/temp/embeddings_{}.pt'.format(chunk))
+
+    # break
