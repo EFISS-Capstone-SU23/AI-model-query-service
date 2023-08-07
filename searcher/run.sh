9ca75a6ee804657502de387c8ebf550630005052
thaiminhpv <thaiminhpv@gmail.com>
2023-08-08 02:33:38 +0700

    multi-step build Dockerfile

--- searcher/run.sh
+++ searcher/run.sh
@@ -12,4 +12,4 @@
     curl -X POST http://localhost:5000/predictions/image-retrieval-v1.0 -T /app/example.json
     echo "Done warming up."
 ) &
-exec torchserve --start --model-store /app/model_store --models image-retrieval-v1.0=model-image-retrieval.mar --ts-config /app/config.properties --foreground
+exec /usr/local/lib/python3.11/site-packages/bin/torchserve --start --model-store /app/model_store --models image-retrieval-v1.0=model-image-retrieval.mar --ts-config /app/config.properties --foreground
