c42d474232bbb95b4b0b0d3ef986103aa0da49cc
thaiminhpv <thaiminhpv@gmail.com>
2023-10-10 23:33:26 +0700

    update pipeline.py

--- indexer/jobs/pipeline.py
+++ indexer/jobs/pipeline.py
@@ -1,9 +1,13 @@
+TOTAL_SHARD = 24
+SHARD_ID = 0
+
 """
 This script uses YOLOv8 to offline crop images and wraps it into a HuggingFace IterableDataset
 """
 
 import numpy as np
 import urllib
+import requests
 import cv2
 import pandas as pd
 import os
@@ -23,12 +27,13 @@ datasets.disable_caching()
 # except RuntimeError:
 #     pass
 
-TOTAL_SHARD = 24
-SHARD_ID = 0
 
 # Define your YOLOv8-related functions here
 def initialize_yolov8_model():
-    model = YOLO('torchscripts_models/yolo/yolov8n_12ep_24-7_32.5mAP.pt')
+    # download https://data.efiss.tech/efiss/yolov8n_12ep_24-7_32.5mAP.pt 
+    # and put it in the current directory
+    urllib.request.urlretrieve('https://data.efiss.tech/efiss/yolov8n_12ep_24-7_32.5mAP.pt', 'yolov8n_12ep_24-7_32.5mAP.pt')
+    model = YOLO('yolov8n_12ep_24-7_32.5mAP.pt')
     model.to('cuda:0')
     return model
 
@@ -51,7 +56,10 @@ def crop_image_with_yolov8(model: YOLO, img: np.ndarray) -> list[np.ndarray]:
 output_dir = 'data/product_images/'
 
 def read_img_from_network(image_path: str) -> np.ndarray:
-    assert image_path.startswith('https://'), f"Image path {image_path} is not a valid URL"
+    # assert image_path.startswith('https://'), f"Image path {image_path} is not a valid URL"
+    prefix = 'https://data.efiss.tech/efiss/'
+    image_path = prefix + image_path
+    # image_path = 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1920&q=80'
     req = urllib.request.urlopen(image_path)
     arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
     img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
@@ -96,11 +104,16 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 ranking_model.eval()
 ranking_model.to(device)
 ...
-
 to_be_index: list[str] = []
-with open('to_be_index_efiss.txt', 'r') as f:
-    for line in f.readlines():
-        to_be_index.append(line.strip())
+# with open('database_info.txt', 'r') as f:
+#     for line in tqdm(f.readlines()):
+#         to_be_index.append(line.strip())
+# read from http
+data_path = 'https://data.efiss.tech/efiss/queue/files_list_efiss.txt'
+import requests
+r = requests.get(data_path, stream=True)
+for line in tqdm(r.iter_lines()):
+    to_be_index.append(line.decode('utf-8').strip())
 
 # Convert to HuggingFace dataset
 dataset = Dataset.from_pandas(pd.DataFrame({'img_path': to_be_index}))
@@ -109,7 +122,7 @@ dataset = dataset.shard(num_shards=TOTAL_SHARD, index=SHARD_ID)
 # dataset = Dataset.from_dict(dataset[:34])
 dataset
 total_len = len(dataset)
-# dataset = dataset.to_iterable_dataset()
+dataset = dataset.to_iterable_dataset()
 cropped_images = dataset.map(crop_image_to_multiple_images, batched=False, remove_columns=['img_path'])
 cropped_images
 # tokenize
@@ -132,13 +145,27 @@ def tokenize_function(row):
         return {'pixel_values': [], 'cropped_img_paths': []}
         
 tokenized_images = cropped_images.map(tokenize_function, batched=True, batch_size=1, remove_columns=['cropped_images'])
-tokenized_images.set_format("torch", device=device)
+tokenized_images = tokenized_images.with_format("torch")
 dataloader = torch.utils.data.DataLoader(tokenized_images, batch_size=16, num_workers=0)
-out = []
-for i, row in enumerate(tqdm(dataloader, total=total_len)):
-    # print(row)
-    out.append(row)
-    if i == 10:
-        break
-# TODO: insert to milvus
-out[0].keys()
+out_cropped_img_paths: list[str] = []
+out_embeddings: list[torch.Tensor] = []
+with torch.no_grad():
+    for i, row in enumerate(tqdm(dataloader, total=total_len)):
+        logits = ranking_model(pixel_values=row['pixel_values']).logits  # (batch_size, 768)
+
+        out_cropped_img_paths.extend(row['cropped_img_paths'])  # list[str]
+        out_embeddings.append(logits.cpu())
+embeddings = torch.cat(out_embeddings, dim=0)
+payload = {
+    'shard_id': SHARD_ID,
+    'embeddings': embeddings.cpu().numpy(),
+    'cropped_img_paths': out_cropped_img_paths
+}
+import pickle
+import requests
+# prepare to send via http
+dumped_payload: bytes = pickle.dumps(payload)
+# send
+url = 'https://indexer.efiss.tech/upload'
+r = requests.post(url, files={'file': ('file.pkl', dumped_payload, 'application/octet-stream')})
+print(r.status_code, r.reason)
